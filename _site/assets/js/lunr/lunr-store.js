40115f490b998893ec35985456754197fccee51b
sprumin <sprumin@gmail.com>
2025-01-12 02:34:44 +0900

    config :: add sidebar

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "Minimal Mistakes 적용기",
         "excerpt":"시즌 525번째 “기술블로그 시작해야지” 개요 매년 이직 시즌이 찾아올 때 마다 기술블로그를 써야겠다는 생각을 한다. 그렇다 진짜 생각만 했다. 이번에 시작안하면 이번 생에는 안할 것 같아서 세팅부터 시작했다. github pages velog, tistory, medium, notion 등 고민을 많이 했지만 github pages로 선택하였다. 각자 장단점이 있지만 github pages를 선택한 이유는 그나마 익숙해서다....","categories": [],
-        "tags": ["github","github pages","ruby","minimal-mistakes"],
+        "tags": ["github","github pages","ruby","minimal mistakes"],
         "url": "/minimal-mistakes/",
         "teaser": null
       }]
